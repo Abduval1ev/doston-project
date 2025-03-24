e40f2cbf96f7b65b8ccdcb1fcb7e74f6c49c547b
Abduval1ev <abduvalievabap@gmail.com>
2025-03-24 23:05:03 +0500

    fix all

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
     const dropdownContainer = document.querySelector(".dropdown-container");
     const dropdownItems = document.querySelectorAll(".dropdown-item");
-    console.log(dropdownContainer);
+    const burgerMenu = document.querySelector(".burger-menu");
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const closeBtn = document.getElementById("close");
 
+    burgerMenu.addEventListener("click", function () {
+        mobileMenu.classList.add("mobile-active");
+    });
+
+    closeBtn.addEventListener("click", function () {
+        mobileMenu.classList.remove("mobile-active");
+    });
 
     dropdownItems.forEach((item) => {
         item.addEventListener("click", (e) => {
             e.stopPropagation();
 
-            if (dropdownContainer.classList.contains("active")) {
-                dropdownContainer.classList.remove("active");
+            if (dropdownContainer.classList.contains("active-nav")) {
+                dropdownContainer.classList.remove("active-nav");
             } else {
-                dropdownContainer.classList.add("active");
+                dropdownContainer.classList.add("active-nav");
             }
         });
     });
 
     document.addEventListener("click", () => {
-        dropdownContainer.classList.remove("active");
+        dropdownContainer.classList.remove("active-nav");
     });
 });

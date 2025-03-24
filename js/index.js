46d26e17a0b6d702969dc48784c6601ed514aad7
Abduval1ev <abduvalievabap@gmail.com>
2025-03-24 23:42:21 +0500

    fix

--- js/index.js
+++ js/index.js
@@ -4,6 +4,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const burgerMenu = document.querySelector(".burger-menu");
     const mobileMenu = document.querySelector(".mobile-menu");
     const closeBtn = document.getElementById("close");
+    const menuItems = document.querySelectorAll(".heading--md");
+
+
+    menuItems.forEach((item) => {
+        item.addEventListener("click", function () {
+            const dropdown = item.querySelector(".mobile-nav-lists");
+
+            if (!dropdown) return;
+
+            const isActive = dropdown.classList.contains("active-nav-list");
+
+            document.querySelectorAll(".mobile-nav-lists").forEach((list) => {
+                list.classList.remove("active-nav-list");
+            });
+
+            if (!isActive) {
+                dropdown.classList.add("active-nav-list");
+            }
+        });
+    });
+
 
     burgerMenu.addEventListener("click", function () {
         mobileMenu.classList.add("mobile-active");
